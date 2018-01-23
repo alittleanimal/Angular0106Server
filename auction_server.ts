553419cf0056ca01516b4c146ad99bcc6aeea914
animal <741996133@qq.com>
2018-01-24 01:11:17 +0800

    add server

--- auction_server.ts
+++ auction_server.ts
@@ -14,6 +14,16 @@ export class Product {
     }
 }
 
+export class Comment {
+    constructor(public id: number,
+        public productId: number,
+        public timestamp: string,
+        public user: string,
+        public rating: number,
+        public content: string) {
+    }
+}
+
 const products: Product[] = [
     new Product(1, '第一个商品', 1.99, 3.5, '这是第一个商品，Angular实战', ['电子产品', '硬件设备']),
     new Product(2, '第二个商品', 2.99, 4.5, '这是第二个商品，Angular实战', ['图书']),
@@ -22,19 +32,32 @@ const products: Product[] = [
     new Product(5, '第五个商品', 5.99, 2.5, '这是第五个商品，Angular实战', ['电子产品', '硬件设备']),
 ];
 
+const comments: Comment[] = [
+    new Comment(1, 1, "2017-02-02 22:22:22", "张三", 3, "good"),
+    new Comment(2, 1, "2017-02-02 22:22:22", "李四", 4, "good"),
+    new Comment(3, 1, "2017-02-02 22:22:22", "王五", 2, "good"),
+    new Comment(4, 2, "2017-02-02 22:22:22", "周六", 5, "good"),
+]
+
 
 app.get('/', (req, res) => {
     res.send("Hello Express");
 })
 
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
     res.json(products);
 })
 
-app.get('/products/:id', (req, res) => {
-    res.json(products.find((product) => 
+app.get('/api/product/:id', (req, res) => {
+    res.json(products.find((product) =>
         product.id == req.params.id
     ));
+})
+
+app.get('/api/product/:id/comments', (req, res) => {
+    res.json(
+        comments.filter((comment: Comment) => comment.productId == req.params.id)
+    );
 })
 
 const serve = app.listen(8000, "localhost", () => {
--- build/auction_server.js
+++ build/auction_server.js
@@ -14,6 +14,18 @@ var Product = /** @class */ (function () {
     return Product;
 }());
 exports.Product = Product;
+var Comment = /** @class */ (function () {
+    function Comment(id, productId, timestamp, user, rating, content) {
+        this.id = id;
+        this.productId = productId;
+        this.timestamp = timestamp;
+        this.user = user;
+        this.rating = rating;
+        this.content = content;
+    }
+    return Comment;
+}());
+exports.Comment = Comment;
 var products = [
     new Product(1, '第一个商品', 1.99, 3.5, '这是第一个商品，Angular实战', ['电子产品', '硬件设备']),
     new Product(2, '第二个商品', 2.99, 4.5, '这是第二个商品，Angular实战', ['图书']),
@@ -21,16 +33,25 @@ var products = [
     new Product(4, '第四个商品', 4.99, 4.5, '这是第四个商品，Angular实战', ['硬件设备']),
     new Product(5, '第五个商品', 5.99, 2.5, '这是第五个商品，Angular实战', ['电子产品', '硬件设备']),
 ];
+var comments = [
+    new Comment(1, 1, "2017-02-02 22:22:22", "张三", 3, "good"),
+    new Comment(2, 1, "2017-02-02 22:22:22", "李四", 4, "good"),
+    new Comment(3, 1, "2017-02-02 22:22:22", "王五", 2, "good"),
+    new Comment(4, 2, "2017-02-02 22:22:22", "周六", 5, "good"),
+];
 app.get('/', function (req, res) {
     res.send("Hello Express");
 });
-app.get('/products', function (req, res) {
+app.get('/api/products', function (req, res) {
     res.json(products);
 });
-app.get('/products/:id', function (req, res) {
+app.get('/api/product/:id', function (req, res) {
     res.json(products.find(function (product) {
         return product.id == req.params.id;
     }));
+});
+app.get('/api/product/:id/comments', function (req, res) {
+    res.json(comments.filter(function (comment) { return comment.productId == req.params.id; }));
 });
 var serve = app.listen(8000, "localhost", function () {
     console.log("服务器已启动， 地址是： http://localhost:8000");
